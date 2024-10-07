80fd7ee59827fbfc4884f4a6c1f38748d06bd69a
unknown <Admin@PC-804.SERVER2.DOMAIN>
2024-10-07 13:12:36 +0800

    Eager Loading and the N+1 Problem

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,10 +10,16 @@ class Job extends Model {
 
     protected $table = 'job_listings';
 
-    protected $fillable = ['title','salary'];
+    protected $fillable = ['title', 'salary'];
 
     public function employer()
     {
-        return $this->belongsTo(employer::class);
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+
     }
 }
